182017ffd4ccef05659dc6b9df5d2e9aa13bfa7d
Vladyslav Nikitiuk <vladnik2k@gmail.com>
2020-03-25 20:49:00 +0200

    Connect the database

--- src/main/java/com/PD/demo/Controller.java
+++ src/main/java/com/PD/demo/Controller.java
@@ -3,10 +3,19 @@ package com.PD.demo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class Controller {
 
+    private final CategoryRepository categoryRepository;
+
+    public Controller(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
     @GetMapping("/start")
-    public void start() {
+    public List<Category> start() {
+        return categoryRepository.findAll();
     }
 }
