5931f2d94962ea2762f064c2a683ec27c3e3128e
Vladyslav Nikitiuk <vladnik2k@gmail.com>
2020-04-20 13:47:26 +0300

    Allow to get category by id with query

--- src/main/java/pd/category/CategoryRepository.java
+++ src/main/java/pd/category/CategoryRepository.java
@@ -5,11 +5,15 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
 
-    @Query(value = "SELECT * from category", nativeQuery = true)
+    @Query(value = "SELECT * FROM category", nativeQuery = true)
     List<Category> findAll();
+
+    @Query(value = "SELECT * FROM category where category.id = ?1", nativeQuery = true)
+    Optional<Category> findById(int id);
 
 }
